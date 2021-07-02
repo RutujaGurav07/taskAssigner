e5115480becd535bd135c21503479b7a2a35784f
Rutuja Gurav <rutujagurav72@gmail.com>
2021-07-02 12:42:14 +0530

    Printing the repostory data of users  on console log

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormControl } from '@angular/forms';
 
-// import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -10,6 +9,7 @@ import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormC
 export class ProfileComponent {
 myform: FormGroup;
 Username:FormControl;
+reqestedUsername;
 
 constructor(private fb: FormBuilder,) { 
 }
@@ -20,22 +20,48 @@ ngOnInit() {
 
 createFormControls() {
   this.Username = new FormControl('', Validators.required);
-  
 }
 
 createForm() {
   this.myform = new FormGroup({
     Username: this.Username,
-    
   });
 }
 
 onSubmit()
 {
   if(this.myform){
-    console.log("Form Submitted");
-    this.myform.reset();
+    requestUserRepos(this.Username.value)
+  }
+
+function requestUserRepos(reqestedUsername){
+  console.log("getting username : ",reqestedUsername)
+  const xhr = new XMLHttpRequest();
+  const url = `https://api.github.com/users/${reqestedUsername}/repos`;
+  xhr.open('GET',url,true);
+
+  xhr.onload = function(){
+
+    const data = JSON.parse(this.response)
+
+    // console.log("getting data ",data);
+    for (let i in data) {
+        
+      // Log the repo name
+      console.log('Repo:', data[i].name);
+      
+      // Log the repo description
+      console.log('Description:', data[i].description);
+      
+      // Log the repo url
+      console.log('URL:', data[i].html_url);
+      
+      // Add a separator between each repo
+      console.log('=========================')
   
+  }
+  }
+  xhr.send();
   }
 }}
 
